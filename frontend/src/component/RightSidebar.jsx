4d0e93b8c7ab1a710084cd05feb4a1274bef714b
Prashant Tiwari <prashanttiwari2585@gmail.com>
2024-07-11 12:49:01 +0530

    who to follow

--- frontend/src/component/RightSidebar.jsx
+++ frontend/src/component/RightSidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import Avatar from "react-avatar";
-function RightSidebar() {
+function RightSidebar({otherUsers}) {
     return (
         <div className='w-[25%] '>
             <div className=' flex items-center p-2 bg-gray-100 rounded-full outline-none w-full '>
@@ -12,23 +12,29 @@ function RightSidebar() {
                 <div className='p-4 bg-gray-100 rounded-2xl my-4'>
                     <h1 className='font-bold text-lg my-3'>Who to follow</h1>
 
-                    <div className='flex items-center justify-between my-2'>
-                        <div className='flex'>
-                            <div>
-                                <Avatar src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg" size="40" round={true} />
+                    {
+                        otherUsers?.map((user) => {
+                            return (
+                                <div key={user?._id} className='flex items-center justify-between my-3'>
+                                    <div className='flex'>
+                                        <div>
+                                            <Avatar src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg" size="40" round={true} />
 
-                            </div>
-                            <div className='ml-2'>
-                                <h1 className='font-bold'>Prashant</h1>
-                                <p className='text-sm'>@prashanttiwari</p>
-                            </div>
-                        </div>
-                        <div>
-                            <button className='px-4 py-1 bg-black text-white rounded-full'>Profile</button>
+                                        </div>
+                                        <div className='ml-2'>
+                                            <h1 className='font-bold'>{user?.name}</h1>
+                                            <p className='text-sm'>{`@${user?.username}`}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className='px-4 py-1 bg-black text-white rounded-full'>Profile</button>
 
-                        </div>
-                    </div>
-                    
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
                 </div>
             </div>
         </div>
