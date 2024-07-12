6e965951bec7e21974d071cb200a622df7522a89
Prashant Tiwari <prashanttiwari2585@gmail.com>
2024-07-12 07:10:15 +0530

    get all tweets

--- frontend/src/hooks/useGetMyTweets.js
+++ frontend/src/hooks/useGetMyTweets.js
@@ -6,37 +6,40 @@ import { getAllTweets } from "../redux/tweetSlice";
 
 const useGetMyTweets = (id) => {
     const dispatch = useDispatch();
-    const { refresh, isActive } = useSelector(store => store.tweet);
-    
+    // const { refresh, isActive } = useSelector(store => store.tweet);
 
-    const fetchMyTweets = async () => {
-        try {
-            const res = await axios.get(`${TWEET_API_END_POINT}/alltweets/${id}`, {
-                withCredentials: true
-            });
-            console.log(res);
-            dispatch(getAllTweets(res.data.tweets));
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    const followingTweetHandler = async () => { 
-        try {
-            axios.defaults.withCredentials = true;
-            const res = await axios.get(`${TWEET_API_END_POINT}/followingtweets/${id}`);
-            console.log(res);
-            dispatch(getAllTweets(res.data.tweets));
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
     useEffect(() => {
-        if(isActive){
-            fetchMyTweets();
-        }else{
-            followingTweetHandler();
+        const fetchMyTweets = async () => {
+            try {
+                const res = await axios.get(`${TWEET_API_END_POINT}/alltweets/${id}`, {
+                    withCredentials: true
+                });
+                console.log(res);
+                dispatch(getAllTweets(res.data.tweets));
+            } catch (error) {
+                console.log(error);
+            }
         }
-    }, [isActive,refresh]);
+        fetchMyTweets();
+    }, [id])
+    // const followingTweetHandler = async () => {
+    //     try {
+    //         axios.defaults.withCredentials = true;
+    //         const res = await axios.get(`${TWEET_API_END_POINT}/followingtweets/${id}`);
+    //         console.log(res);
+    //         dispatch(getAllTweets(res.data.tweets));
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     if (isActive) {
+    //         fetchMyTweets();
+    //     } else {
+    //         followingTweetHandler();
+    //     }
+    // }, [isActive, refresh]);
 };
 export default useGetMyTweets;
--- frontend/src/redux/tweetSlice.js
+++ frontend/src/redux/tweetSlice.js
@@ -1,38 +1,21 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userSlice from "./userSlice";
-import tweetSlice from "./tweetSlice";
-
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-
-const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-}
-
-const rootReducer = combineReducers({
-    user: userSlice,
-    tweet: tweetSlice,
-})
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+import {createSlice} from "@reduxjs/toolkit";
+const tweetSlice = createSlice({
+    name:"tweet",
+    initialState:{
+        tweets:null
+    },
+    reducers:{
+        getAllTweets:(state,action)=>{
+            state.tweets = action.payload;
+        }
+        // ,
+        // getRefresh:(state)=>{
+        //     state.refresh = !state.refresh;
+        // },
+        // getIsActive:(state,action)=>{
+        //     state.isActive = action.payload;
+        // }
+    }
 });
-export default store;
+export const {getAllTweets} = tweetSlice.actions;
+export default tweetSlice.reducer;
