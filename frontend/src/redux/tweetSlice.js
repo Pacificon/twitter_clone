11c7876421ad75fc6c23e9f692796b7177173a1f
Prashant Tiwari <prashanttiwari2585@gmail.com>
2024-07-12 10:54:13 +0530

    user tweets and following tweets, delete button

--- frontend/src/redux/tweetSlice.js
+++ frontend/src/redux/tweetSlice.js
@@ -1,21 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 const tweetSlice = createSlice({
-    name:"tweet",
-    initialState:{
+    name: "tweet",
+    initialState: {
         tweets: null,
-        refresh:false
+        refresh: false,
+        isActive: true
     },
-    reducers:{
-        getAllTweets:(state,action)=>{
+    reducers: {
+        getAllTweets: (state, action) => {
             state.tweets = action.payload;
         },
-        getRefresh:(state)=>{
+        getRefresh: (state) => {
             state.refresh = !state.refresh;
         },
-        // getIsActive:(state,action)=>{
-        //     state.isActive = action.payload;
-        // }
+        getIsActive: (state, action) => {
+            state.isActive = action.payload;
+        }
     }
 });
-export const {getAllTweets,getRefresh} = tweetSlice.actions;
+export const { getAllTweets, getRefresh, getIsActive } = tweetSlice.actions;
 export default tweetSlice.reducer;
